737fb41505843ec5871c5fc46f2070dfb6acc1c2
kripi-png <66375160+kripi-png@users.noreply.github.com>
2021-02-11 19:04:36 +0200

    searching for a game works

--- main.py
+++ main.py
@@ -1,4 +1,53 @@
-import tkinter as tk
-#Importing the main module
-window = tk.Tk()
-window.mainloop()
+import requests, json
+import PySimpleGUI as sg
+
+def main():
+    sg.theme('DarkAmber')
+    layout = [  [sg.Text('Search for a game (min 3 characters):'), sg.InputText(enable_events=True, key='gameNameInput')],
+                [sg.Listbox([], size=(40,5), key='gameList', enable_events=True)],
+                [sg.Text("Game Selected:"), sg.Text('', key='gameSelectedText', size=(50,1))],
+                [sg.Button("Ok", key='ok'), sg.Button("Cancel", key='cancel')] ]
+
+    window = sg.Window("disGame", layout)
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+
+        if event == sg.WIN_CLOSED or event == 'cancel': # if user closes window or clicks cancel
+            break
+
+        if event == 'gameNameInput' and len(values['gameNameInput']) >= 3:
+            print("Looking for ", values['gameNameInput'])
+            data = filterData(values['gameNameInput'])
+            print(data)
+            if data:
+                window['gameList'].update([x for x in data])
+            else: window['gameList'].update([])
+
+        elif event == 'gameList' and len(values['gameList']):
+            window['gameSelectedText'].update(value=values['gameList'][0])
+
+    window.close()
+
+def loadData():
+    """load data and remove all unnecessary info"""
+    r = requests.get('https://discord.com/api/v8/applications/detectable')
+    r = r.json()
+    data = {}
+    for item in r:
+        data[item["name"]] = item["id"]
+    return data
+
+def filterData(query):
+    """create and return a new dict of values name of which matches with the query"""
+    returnData = {}
+    for item in DATA:
+        if query.lower() in item.lower():
+            returnData[str(item)] = DATA[item]
+    return returnData
+
+if __name__ == '__main__':
+    global DATA
+    DATA = loadData()
+    main()
